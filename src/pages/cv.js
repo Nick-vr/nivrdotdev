4786489ae444771d4cad5ee0816196c466398d02
Nick-vr <nvanroyen@gmail.com>
2021-03-10 14:39:34 +0100

    CV

--- src/pages/cv.js
+++ src/pages/cv.js
@@ -64,7 +64,7 @@ const SDownloadCV = styled.div`
   img {
     margin-right: 20px;
     color: #373737;
-    width: 1.5rem;
+    width: 4rem;
     filter: invert(30%);
     transition: filter 0.5s ease-in-out;
 
@@ -89,7 +89,7 @@ const CvPage = () => {
         <BaseWrapper>
           <h2>/Education</h2>
 
-          <h3>_Vdab & Obelisk</h3>
+          <h3>Vdab & Obelisk</h3>
           <p>.Net developer with C# | Currently enrolled</p>
           <SIndentedP>
             SQL, UML, C#, Entity framework, ASP.NET (Core) MVC,REST, Test driven
@@ -97,13 +97,13 @@ const CvPage = () => {
             SCRUM
           </SIndentedP>
 
-          <h3>_Multimedia 3rd degree</h3>
+          <h3>Multimedia 3rd degree</h3>
           <p>VISO Gent | 2004 to 2006</p>
 
-          <h3>_Mechanical techniques 2nd degree</h3>
+          <h3>Mechanical techniques 2nd degree</h3>
           <p>Sint-Joris Bazel | 2002 to 2004</p>
 
-          <h3>_Industrial sciences 2nd degree</h3>
+          <h3>Industrial sciences 1st degree</h3>
           <p>VTS2 Sint-Niklaas | 2000 to 2002</p>
         </BaseWrapper>
 
@@ -111,25 +111,25 @@ const CvPage = () => {
         <BaseWrapper>
           <h2>/Work</h2>
 
-          <h3>_Developer</h3>
+          <h3>Developer</h3>
           <p>Puls | 2020 to 2020</p>
           <SIndentedP>Worked on in-house framework.</SIndentedP>
 
-          <h3>_Technical Draftsman</h3>
+          <h3>Technical Draftsman</h3>
           <p>Admibo bvba | 2017 to 2020</p>
           <SIndentedP>
             Drawing surfaces for road works and diversions. Staying incontact
             with the customers to make adjustments.
           </SIndentedP>
 
-          <h3>_3D Printing</h3>
+          <h3>3D Printing</h3>
           <p>Materialise | 2015 to 2017</p>
           <SIndentedP>
             Accepting orders and working with 3D printed materialsfrom start to
             finish.
           </SIndentedP>
 
-          <h3>_HVAC Installer</h3>
+          <h3>HVAC Installer</h3>
           <p>T.L. bvba & Borcalor NV | 2007 to 2014</p>
           <SIndentedP>
             Install, repair, replace and maintain HVAC systems for homesand
